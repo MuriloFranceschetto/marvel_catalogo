9f4b19fca3e8752a4bdf95efaba6ffaddf1b7ff8
Murilo Franceschetto <gfmurilogf@gmail.com>
2021-05-20 22:45:38 -0300

    Terminado detalhes do personagem

--- lib/views/personagem-details.dart
+++ lib/views/personagem-details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:marvel/components/Title.dart';
 import 'package:marvel/models/response-marvel.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PersonagemDetails extends StatelessWidget {
   final Results personagem;
@@ -20,11 +21,11 @@ class PersonagemDetails extends StatelessWidget {
             child: Text(this.personagem.description,
                 style: TextStyle(color: Colors.white)),
           ),
-          Card(
-            child: _buildComics(),
-            color: Colors.black12,
-          ),
+          Card(child: _buildComics(), color: Colors.black12),
           Card(child: _buildEvents(), color: Colors.black12),
+          Card(child: _buildStories(), color: Colors.black12),
+          Card(child: _buildSeries(), color: Colors.black12),
+          Card(child: _buildOthers(), color: Colors.black12)
         ],
       ),
     );
@@ -75,4 +76,86 @@ class PersonagemDetails extends StatelessWidget {
       return;
     }
   }
+
+  _buildStories() {
+    if (this.personagem.stories.returned > 0) {
+      return new Column(
+        children: <Widget>[
+          MyTitle('Histórias'),
+          ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: personagem.stories.returned,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(personagem.stories.items[index].name,
+                    style: TextStyle(color: Colors.white, fontSize: 13)),
+              );
+            },
+          )
+        ],
+      );
+    } else {
+      return;
+    }
+  }
+
+  _buildSeries() {
+    if (this.personagem.series.returned > 0) {
+      return new Column(
+        children: <Widget>[
+          MyTitle('Séries'),
+          ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: personagem.series.returned,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(personagem.series.items[index].name,
+                    style: TextStyle(color: Colors.white, fontSize: 13)),
+              );
+            },
+          )
+        ],
+      );
+    } else {
+      return;
+    }
+  }
+
+  _buildOthers() {
+    if (this.personagem.series.returned > 0) {
+      return new Column(
+        children: <Widget>[
+          MyTitle('Mais informações em:'),
+          ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: personagem.urls.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                onTap: () {
+                  _launchURL(personagem.urls[index].url);
+                },
+                title: Text(
+                  personagem.urls[index].type,
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  personagem.urls[index].url,
+                  style: TextStyle(color: Colors.white),
+                ),
+              );
+            },
+          )
+        ],
+      );
+    } else {
+      return;
+    }
+  }
+
+  void _launchURL(_url) async => await canLaunch(_url)
+      ? await launch(_url)
+      : throw 'Could not launch $_url';
 }
